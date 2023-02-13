e91ad3038ad8b2d84dc25bd5f5c85c4635e5f6af
prabhathc <17132210+prabhathc@users.noreply.github.com>
2023-02-13 01:16:09 -0500

    featuer(frontend): more updates

--- client/src/components/Learn.jsx
+++ client/src/components/Learn.jsx
@@ -1,53 +1,56 @@
 import React from "react";
+import Tilt from "react-parallax-tilt";
+import { useState, useEffect } from "react";
+import soundcloud from "../assets/soundcloud.png";
+import youtube from "../assets/youtube.png";
+import Footer from "./Footer";
 
 export default function Learn() {
 
-    const features = [
-        {
-          name: 'Competitive exchange rates',
-          description:
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-        },
-        {
-          name: 'No hidden fees',
-          description:
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-        },
-        {
-          name: 'Transfers are instant',
-          description:
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-        },
-        {
-          name: 'Mobile notifications',
-          description:
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-        },
-      ]
-    return (
-        <div className="relative px-6 lg:px-8 h-screen">
-          <div className="mx-auto max-w-screen-xl pt-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-            <div className="flex-auto bg-pillwhite border-2 rounded-lg shadow-lg hover:border-pillred bg-pillred/20">
-              <div className="flex justify-center font-bold text-2xl tracking-tight text-pillred p-4">
-                Play unlimited music from Soundcloud
-              </div>
-            </div>
-            <div className="flex-auto bg-pillred border-2 border-pillwhite rounded-lg">
-              <div className="flex justify-center font-bold text-2xl tracking-tight text-pillwhite p-4">
-                Play unlimited music from Youtube
-              </div>
-            </div>
-            <div className="flex-auto bg-pillred rounded-lg">
-              <div className="flex justify-center font-bold text-2xl tracking-tight text-pillwhite p-4">
-                All with a few simple commands
-              </div>
-            </div>
-            <div className="flex-auto bg-pillwhite border-2 rounded-lg shadow-lg hover:border-pillred bg-pillred/20">
-              <div className="flex justify-center font-bold text-2xl tracking-tight text-pillred p-4 ">
-                All for free!
-              </div>
-            </div>
+  const [width, setWidth] = useState(window.innerWidth);
+
+  function handleWindowSizeChange() {
+      setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+      window.addEventListener('resize', handleWindowSizeChange);
+      return () => {
+          window.removeEventListener('resize', handleWindowSizeChange);
+      }
+  }, []);
+
+  return (
+    <div className="relative h-screen px-6 lg:px-8 py-4 pt-4 grid gap-4 sm:gap-6 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 justify-items-stretch">
+      <div className="flex">
+        <Tilt className="bg-pillwhite/20 rounded-3xl shadow-lg" tiltEnable={width > 768} tiltReverse={true} perspective={2000} glareEnable={false} glareMaxOpacity={0.2} glareBorderRadius="24px" transitionSpeed={1000} gyroscope={true} tiltMaxAngleX={0}>
+          <div className="font-bold text-3xl md:text-4xl tracking-tight text-pillwhite p-6">
+            Play unlimited music from Soundcloud
           </div>
-        </div>
-    )
+          {/* <img className="px-6 pb-6 h-24 flex flex-row justify-center" src={soundcloud} alt="" /> */}
+        </Tilt>
+      </div>
+      <div className="flex">
+        <Tilt className="bg-pillwhite/20 rounded-3xl shadow-lg" tiltEnable={width > 768} tiltReverse={true} perspective={2000} glareEnable={false} glareMaxOpacity={0.2} glareBorderRadius="24px" transitionSpeed={1000} gyroscope={true} tiltMaxAngleX={0}>
+          <div className="font-bold text-3xl md:text-4xl tracking-tight text-pillwhite p-6">
+            Play unlimited music from Youtube
+          </div>
+          {/* <img className="px-6 pb-6 h-24 flex flex-row justify-center" src={youtube} alt="" /> */}
+        </Tilt>
+      </div>
+      <div className="flex">
+        <Tilt className="bg-pillwhite/20 rounded-3xl shadow-lg" tiltEnable={width > 768} tiltReverse={true} perspective={2000} glareEnable={false} glareMaxOpacity={0.2} glareBorderRadius="24px" transitionSpeed={1000} gyroscope={true} tiltMaxAngleX={0}>
+          <div className="font-bold text-2xl tracking-tight text-pillwhite p-6">
+            Play unlimited music from Soundcloud
+          </div>
+        </Tilt>
+      </div>
+      <div className="flex">
+        <Tilt className="bg-pillwhite/20 rounded-3xl shadow-lg" tiltEnable={width > 768} tiltReverse={true} perspective={2000} glareEnable={false} glareMaxOpacity={0.2} glareBorderRadius="24px" transitionSpeed={1000} gyroscope={true} tiltMaxAngleX={0}>
+          <div className="font-bold text-2xl tracking-tight text-pillwhite p-6">
+            Play unlimited music from Youtube
+          </div>
+        </Tilt>
+      </div>
+    </div>
+  )
 }
